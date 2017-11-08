311fbcc6581d5ae826a0213744d3fa41f6e88c79
xianhua <xianhua@meizu.com>
2017-11-08 10:40:20 +0800

    'rdma'

--- infiniband_install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-yum install -y rdma libibverbs libmlx5 libmlx4 infinipath-psm librdmacm-utils librdmacm ibacm infiniband-diags libibverbs-utils opensm
-
-systemctl enable rdma
