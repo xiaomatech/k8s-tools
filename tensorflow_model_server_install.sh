3774e2ceb93bad255f38c53f0c54354bdadd0757
xianhua <xianhua@meizu.com>
2018-01-16 17:23:05 +0800

    'init'

--- tensorflow_model_server_install.sh
+++ tensorflow_model_server_install.sh
@@ -5,7 +5,7 @@ wget https://github.com/bazelbuild/bazel/releases/download/$bazel_version/bazel-
 bash ./bazel-$bazel_version-installer-linux-x86_64.sh
 source /usr/local/lib/bazel/bin/bazel-complete.bash
 export PATH="$PATH:/usr/local/lib/bazel/bin"
-yum install -y freetype-devel libcurl-devel gcc-c++ libpng-devel python-devel python-pip numpy zip libzip-devel bzip2-devel
+yum install -y freetype-devel libcurl-devel gcc-c++ libpng-devel python-devel python-pip numpy zip libzip-devel bzip2-devel jemalloc
 git clone --recursive  https://github.com/tensorflow/serving
 cd serving
 cd tensorflow
