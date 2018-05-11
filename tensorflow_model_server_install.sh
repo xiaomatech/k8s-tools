5bbe6cfa3372a78ca5950f274b21c05e523b15a5
xianhuawei <258022429@qq.com>
2018-05-11 14:48:27 +0800

    Update tensorflow_model_server_install.sh

--- tensorflow_model_server_install.sh
+++ tensorflow_model_server_install.sh
@@ -11,6 +11,11 @@ cd serving
 cd tensorflow
 ./configure
 cd ..
-bazel build -c opt --copt=-msse4.1 --copt=-msse4.2 --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-O3 tensorflow_serving/...
+bazel build -c opt --copt=-msse4.1 --copt=-msse4.2 --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-O3 tensorflow_serving/...
 
 ll bazel-bin/tensorflow_serving/model_servers/tensorflow_model_server
+
+
+# rpm -ivh https://copr-be.cloud.fedoraproject.org/results/croberts/bazel/epel-7-x86_64/00745932-bazel/bazel-0.11.1-1.el7.centos.x86_64.rpm
+
+# rpm -ivh https://copr-be.cloud.fedoraproject.org/results/croberts/tensorflow-model-server/epel-7-x86_64/00751750-tensorflow-model-server/tensorflow-model-server-1.6.0-2.el7.centos.x86_64.rpm
