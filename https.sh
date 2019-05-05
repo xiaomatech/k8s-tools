7ee158e23a15306b2da7c3d9164221fd60db19b3
xianhuawei <258022429@qq.com>
2019-05-05 10:36:01 +0800

    Update https.sh

--- https.sh
+++ https.sh
@@ -1,7 +1,10 @@
-wget https://dl.eff.org/certbot-auto -O /usr/sbin/certbot-auto
-chmod a+x /usr/sbin/certbot-auto
+#!/usr/bin/env bash
+
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
+
+wget https://dl.eff.org/certbot-auto -O /usr/sbin/certbot-auto
+chmod a+x /usr/sbin/certbot-auto
 
 #把同一个域名的多个站点，生成到一个证书里面
 /usr/sbin/certbot-auto certonly --email user@example.com  --agree-tos --no-eff-email --webroot \
@@ -15,7 +18,6 @@ export LC_CTYPE="en_US.UTF-8"
 -w /data/www/api.example.com/public/api -d api.example.com
 
 #默认情况下，let's encripty 会把pem文件生成到/etc/letsencript/live/domain下面
-
 
 #配置 certbot 自动更新证书
 /usr/sbin/certbot-auto renew --quite --post-hook "/etc/init.d/nginx reload"
